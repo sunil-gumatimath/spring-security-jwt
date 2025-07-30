461fda75aa156e67063e7cc196b4af072259af82
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-07-31 00:31:47 +0530

    feat: Configure default Spring Security user credentials

---
+++ src/main/java/com/springsecurity/HelloController.java
@@ -0,0 +1,14 @@
+package com.springsecurity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome to TED " + request.getSession().getId();
+    }
+}
